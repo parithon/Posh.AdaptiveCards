ca459a3eac5c58041e020c0cee8a515b9d0d9824
Anthony Conrad <parithon@outlook.com>
2021-09-12 16:47:56 -0700

    Added ConvertTo-AdaptiveCardPayload

---
+++ Posh.AdaptiveCards.Models/TeamsAdaptiveCard.cs
@@ -0,0 +1,11 @@
+﻿using AdaptiveCards;
+
+namespace Posh.AdaptiveCards.Models
+{
+    public class TeamsAdaptiveCard
+    {
+        public string ContentType { get; } = AttachmentContentTypes.AdaptiveCard;
+        public string ContentUrl { get; } = string.Empty;
+        public AdaptiveCard Content { get; set; }
+    }
+}
---
+++ Posh.AdaptiveCards.Models/TeamsMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Posh.AdaptiveCards.Models
+{
+    public class TeamsMessage
+    {
+        public string Type { get; } = "message";
+        public IEnumerable<TeamsAdaptiveCard> Attachments { get; } = new Collection<TeamsAdaptiveCard>();
+    }
+}
